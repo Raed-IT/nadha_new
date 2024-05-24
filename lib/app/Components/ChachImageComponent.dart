8e9a7bc24150acc3e289172d30b54b4660748147
Raed-It <raedswanit@gmail.com>
2024-05-24 22:58:56 +0300

    fix images caches (app crache on replace)

--- lib/app/Components/ChachImageComponent.dart
+++ lib/app/Components/ChachImageComponent.dart
@@ -1,9 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:card_loading/card_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
+import 'package:fast_cached_network_image/fast_cached_network_image.dart';
 
 class ImageCacheComponent extends StatelessWidget {
   final String image;
@@ -28,7 +26,48 @@ class ImageCacheComponent extends StatelessWidget {
       width: width,
       child: ClipRRect(
         borderRadius: borderRadius ?? BorderRadius.zero,
-        child: CachedNetworkImage(
+        child: FastCachedImage(
+          url: image,
+          cacheHeight: 290.sp.toInt(),
+          cacheWidth: 290.sp.toInt(),
+          fit: fit??BoxFit.contain,
+          width: width,
+          height: height,
+          loadingBuilder: (context, progress) {
+            return Material(
+              child: SizedBox(
+                width: width,
+                height: height,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: Opacity(
+                        opacity: 0.2,
+                        child: Padding(
+                          padding: EdgeInsets.all(20.sp),
+                          child: Image.asset("assets/images/loader.png"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: AutoSizeText(
+                          maxLines: 1,
+                          "${ progress.progressPercentage.value==0?'':progress.progressPercentage.value} %"),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+/*
+CachedNetworkImage(
           color: imageColor,
           fit: fit ?? BoxFit.cover,
           height: height,
@@ -39,44 +78,24 @@ class ImageCacheComponent extends StatelessWidget {
               child: SizedBox(
                 width: width,
                 height: height,
-                child: Stack(
+                child: Column(
                   children: [
-                    Opacity(
-                      opacity: 0.3,
-                      child: CardLoading(
-                        height: height ?? Get.height,
-                        cardLoadingTheme: CardLoadingTheme(
-                            colorOne: Theme.of(context)
-                                .colorScheme
-                                .primary
-                                .withOpacity(0.1),
-                            colorTwo: Colors.black26),
-                        borderRadius: borderRadius ??
-                            BorderRadius.all(
-                              Radius.circular(10.sp),
-                            ),
+                    Expanded(
+                      flex: 4,
+                      child: Opacity(
+                        opacity: 0.2,
+                        child: Padding(
+                          padding: EdgeInsets.all(20.sp),
+                          child: Image.asset("assets/images/loader.png"),
+                        ),
                       ),
                     ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            flex: 4,
-                            child: Padding(
-                              padding: EdgeInsets.all(20.sp),
-                              child: Image.asset("assets/images/loader.png"),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 1,
-                            child: AutoSizeText(
-                                maxLines: 1,
-                                "${((progress.progress ?? 0) * 100).toStringAsFixed(2)} %"),
-                          )
-                        ],
-                      ),
-                    ),
+                    Expanded(
+                      flex: 1,
+                      child: AutoSizeText(
+                          maxLines: 1,
+                          "${((progress.progress ?? 0) * 100).toStringAsFixed(2)} %"),
+                    )
                   ],
                 ),
               ),
@@ -91,8 +110,6 @@ class ImageCacheComponent extends StatelessWidget {
               ),
             );
           },
-        ),
-      ),
-    );
-  }
-}
+        )
+
+* */
